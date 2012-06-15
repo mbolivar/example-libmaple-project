21cd0d7ab786a63a7f9a5c529c148897e7e1d51f
Marti Bolivar <mbolivar@leaflabs.com>
2012-06-15 00:15:20 -0400

    main.cpp: Add reference to real-main.cpp.
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * This file intentionally left empty.
+ * This file intentionally left empty. See real-main.cpp.
  *
  * The libmaple build system stupidly insists that you really didn't
  * want to build main.cpp from anywhere other than the root directory
